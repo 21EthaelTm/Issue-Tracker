ccd9320538a8a588d2bcc72bb25530df6d100cb6
bilisuma <21bilisuma@gmail.com>
2025-11-27 00:24:57 +0300

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,56 @@
-'use client'
-import React from 'react'
-import { TextField ,TextArea, Button} from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
-// import SimpleMDE from "react-simplemde-editor";
+"use client";
+import React, { useState } from "react";
+import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { error } from "console";
 
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false // ← This is the key difference
+interface Issuetype {
+  title: string;
+  description: string;
+}
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false, // ← This is the key difference
 });
 const newIssuPage = () => {
+  const [error, serError] = useState("");
+  const { register, handleSubmit, control } = useForm<Issuetype>();
+  const router = useRouter();
   return (
-    <div className='max-w-xl space-y-3'>
-    <TextField.Root placeholder="Title" >
-    </TextField.Root>
-    <SimpleMDE />;
-    <Button>Submit New Issue</Button>
-    
-    </div>
-  )
-}
+    <div className="max-w-xl ">
+      {error && <Callout.Root color="red" className="mb-5">
+         <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>}
 
-export default newIssuPage
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issue", data);
+            router.push("/issues");
+          } catch (error) {
+            serError('unexpected error occured ')
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          // Simplified render prop passing all field props directly
+          render={({ field }) => <SimpleMDE placeholder="description" {...field} />}
+        />
+
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default newIssuPage;

2aa65a4607ebb1ce7be225fe36c134e9f3d84dd9
bilisuma <21bilisuma@gmail.com>
2025-11-27 11:57:02 +0300

    import prisma

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import {Button} from '@radix-ui/themes'
-import Link from 'next/link'
-const IssuePage = () => {
-  return (
-    <div><Button><Link href='/issues/new'>New Issues</Link></Button></div>
-  )
-}
+import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import TableComponent from "../components/Tablecomponent";
+//import { prisma } from "@/prisma/client";
+const IssuePage = async () => {
+ // const submitedIssues = await prisma.issue.findMany();
 
-export default IssuePage
+  return (
+    <div>
+      <div className="mb-3">
+      <Button>
+        <Link href="/issues/new">New Issues</Link>
+      </Button>
+      </div>
+      <TableComponent/>
+    </div>
+  );
+};
+
+export default IssuePage;

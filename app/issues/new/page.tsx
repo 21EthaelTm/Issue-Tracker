26ff145e1265528e5fdf3b0dfa7101a5d81bc79e
bilisuma <21bilisuma@gmail.com>
2025-11-27 10:10:42 +0300

    create type from zod Schema

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,24 +6,31 @@ import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import {z} from 'zod';
-import {zodResolver } from "@hookform/resolvers/zod";
-import {createIssueSchema} from '../../validationSchema'
-
-type Issuetype = z.infer<typeof createIssueSchema>
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "../../validationSchema";
+import ErrorMessage from "../../components/ErrorMessage";
+type Issuetype = z.infer<typeof createIssueSchema>;
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false, // ← This is the key difference
 });
 const newIssuPage = () => {
   const [error, setError] = useState("");
-  const { register, handleSubmit, control,formState :{errors} } = useForm<Issuetype>({resolver:zodResolver(createIssueSchema)});
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Issuetype>({ resolver: zodResolver(createIssueSchema) });
   const router = useRouter();
   return (
     <div className="max-w-xl ">
-      {error && <Callout.Root color="red" className="mb-5">
-         <Callout.Text>{error}</Callout.Text>
-      </Callout.Root>}
-      
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
       <form
         className=" space-y-3"
         onSubmit={handleSubmit(async (data) => {
@@ -31,7 +38,7 @@ const newIssuPage = () => {
             await axios.post("/api/issue", data);
             router.push("/issues");
           } catch (error) {
-            setError('unexpected error occured ')
+            setError("unexpected error occured ");
           }
         })}
       >
@@ -39,14 +46,17 @@ const newIssuPage = () => {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
-         {errors.title && <Text color="red">{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
         <Controller
           name="description"
           control={control}
           // Simplified render prop passing all field props directly
-          render={({ field }) => <SimpleMDE placeholder="description" {...field} />}
+          render={({ field }) => (
+            <SimpleMDE placeholder="description" {...field} />
+          )}
         />
-        {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit New Issue</Button>
       </form>

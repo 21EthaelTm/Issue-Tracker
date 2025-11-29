2ce3817f56474cc8924090dfd5a7ae6a8d560774
bilisuma <21bilisuma@gmail.com>
2025-11-29 13:44:00 +0300

    show conformation dialog box

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/prisma/client";
-import { Box, Flex, Grid } from "@radix-ui/themes";
+import { AlertDialog, Box, Button, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
@@ -24,7 +24,30 @@ const displaySpecificIssue = async ({
       <Box>
         <Flex direction="column" gap="4">
         <EditIssueButton IssueId={issue.id}/>
+        <AlertDialog.Root>
+	       <AlertDialog.Trigger>
         <DeleteIssueButton IssueId={issue.id}/>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description >
+			Are you sure you want to Delete this Isseu? once deteled it cant be undone!
+		</AlertDialog.Description>
+        
+        <Flex gap="3" mt="4" justify="center">
+			<AlertDialog.Cancel>
+				<Button variant="soft" color="gray">
+					Cancel
+				</Button>
+			</AlertDialog.Cancel>
+			<AlertDialog.Action>
+				<Button variant="solid" color="red">
+					Delete Issue
+				</Button>
+			</AlertDialog.Action>
+		</Flex>
+    </AlertDialog.Content>
+        </AlertDialog.Root>
         </Flex>
       </Box>
     </Grid>

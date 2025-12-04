d73f3fb90fd5f03c59e06b54648a4a7b60f63ef4
bilisuma <21bilisuma@gmail.com>
2025-12-04 11:52:07 +0300

    add Metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,31 +7,39 @@ import IssueDetails from "./IssueDetails";
 import { getServerSession } from "next-auth";
 import authoption from "@/app/auth/authoption";
 import AssignSelect from "./AssigneeSelect";
-const displaySpecificIssue = async ({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) => {
-  const session =await getServerSession(authoption)
+const displaySpecificIssue = async ({ params }:{ params: Promise<{ id: string }> }) => {
+  const session = await getServerSession(authoption);
   const id = (await params).id;
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
   if (!issue) notFound();
   return (
-    <Grid columns={{ initial: "1", sm: "5" }} gap='3'>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="3">
       <Box className="md:col-span-4">
-      <IssueDetails issue={issue}/>
+        <IssueDetails issue={issue} />
       </Box>
-      { session && <Box>
-        <Flex direction="column" gap="4">
-          <AssignSelect issue={issue}/>
-        <EditIssueButton IssueId={issue.id}/>
-        <DeleteIssueButton IssueId={issue.id}/>
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssignSelect issue={issue} />
+            <EditIssueButton IssueId={issue.id} />
+            <DeleteIssueButton IssueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
 
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+const id =parseInt((await params).id);
+ const issue = await prisma.issue.findUnique({where:{id}})
+ return(
+  {
+    title:issue?.title,
+    description:'Detail of issues' + issue?.title
+  }
+ )
+}
 export default displaySpecificIssue;

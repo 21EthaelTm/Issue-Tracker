258dac9d431320e7e741680f6f5788f46c6c21c5
bilisuma <21bilisuma@gmail.com>
2025-12-04 10:06:54 +0300

    layout the dashBord

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,19 @@ import IatestIssueComponent from './IatestIssueComponent'
 import IssueSummary from "./IssueSummary";
 import { prisma } from "@/prisma/client";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 export default async function Home() {
  const openCount =await prisma.issue.count({where:{status:"open"}})
  const ClosedCount = await prisma.issue.count({where:{status:"closed"}})
  const In_ProgressCount = await prisma.issue.count({where:{status:"In_progress"}})
   return (
-    <IssueChart closed={ClosedCount} inProgress={In_ProgressCount} open={openCount}/>
-    //<IatestIssueComponent/>
+    <Grid columns={{initial:"1",md:"2"}} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary closed={ClosedCount} inProgress={In_ProgressCount} open={openCount}/>
+      <IssueChart closed={ClosedCount} inProgress={In_ProgressCount} open={openCount}/>
+      </Flex>
+       <IatestIssueComponent/>
+    </Grid>
+    //
   );
 }
